94afba11b5463175dbd7cefb4d1e7d7c3e47b451
pszachta <pszachta@umich.edu>
2024-06-17 18:13:08 -0400

    Loaded hosp admissions data into SQL table in csv_to_sql_hosp_admissions() function

--- llm.py
+++ llm.py
@@ -23,7 +23,7 @@ def llama3(prompt):
     return(response.json()['message']['content'])
 
 
-def try_llama3():
+def attempt_llama3():
     num1 = int(llama3("Pick a number from 1 to 10. Say only the number and nothing else."))
     print(num1)
     num2 = int(llama3("Pick a number from 1 to 10, besides 7. Say only the number and nothing else."))
@@ -32,12 +32,28 @@ def try_llama3():
 
 # https://stackoverflow.com/questions/2887878/importing-a-csv-file-into-a-sqlite3-database-table-using-python
 def csv_to_sql_hosp_admissions():
-    con = sqlite3.connect("sqlite:///mimic-iv/hosp/admissions.csv.gz")
+    con = sqlite3.connect("admissions.db")
     cur = con.cursor()
-    cur.execute("CREATE TABLE t (subject_id, hadm_id, admittime, dischtime, deathtime, admission_type, admit_provider_id, admission_location, discharge_location, insurance, language, marital_status, race, edregtime, edouttime, hospital_expire_flag);")
-    with open('data.csv','r') as fin:
-        dr = csv.DictReader(fin)
-        to_db = [(i['subject_id'], i['hadm_id'], i['admittime'], i['dischtime'], i['deathtime'], i['admission_type'], i['admit_provider_id'], i['admission_location'], i['discharge_location'], i['insurance'], i['language'], i['marital_status'], i['race'], i['edregtime'], i['edouttime'], i['hospital_expire_flag']) for i in dr]
-    cur.executemany("INSERT INTO t (subject_id, hadm_id, admittime, dischtime, deathtime, admission_type, admit_provider_id, admission_location, discharge_location, insurance, language, marital_status, race, edregtime, edouttime, hospital_expire_flag) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", to_db)
-    con.commit()
+    cur.execute("DROP TABLE IF EXISTS admissions;")
+    cur.execute("CREATE TABLE admissions (subject_id, hadm_id, admittime, dischtime, deathtime, admission_type, admit_provider_id, admission_location, discharge_location, insurance, language, marital_status, race, edregtime, edouttime, hospital_expire_flag);")
+    with open('mimic-iv-2.2/hosp/admissions.csv', 'r') as file:
+        total_lines = len(file.readlines())
+    with open('mimic-iv-2.2/hosp/admissions.csv', 'r') as file:
+        current_line_num = 1
+        while current_line_num <= total_lines:
+            current_line = file.readline().split(',')
+            if current_line_num > 1:
+                print(current_line_num)
+                subject_id, hadm_id, admittime, dischtime, deathtime, admission_type, admit_provider_id, admission_location, discharge_location, insurance, language, marital_status, race, edregtime, edouttime, hospital_expire_flag = current_line
+                hospital_expire_flag = hospital_expire_flag.split('\n')[0]
+                cur.execute("INSERT INTO admissions (subject_id, hadm_id, admittime, dischtime, deathtime, admission_type, admit_provider_id, admission_location, discharge_location, insurance, language, marital_status, race, edregtime, edouttime, hospital_expire_flag) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
+                            (subject_id, hadm_id, admittime, dischtime, deathtime, admission_type, admit_provider_id, admission_location, discharge_location, insurance, language, marital_status, race, edregtime, edouttime, hospital_expire_flag))
+                con.commit()
+            current_line_num += 1
+    cur = con.cursor()
+    cur.execute("SELECT * FROM admissions;")
+    for row in cur.fetchall():
+        print(row)
     con.close()
+
+csv_to_sql_hosp_admissions()
