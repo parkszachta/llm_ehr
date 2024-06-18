211ceb0e638ee26a70286269f504c7e60c26d467
pszachta <pszachta@umich.edu>
2024-06-18 15:11:27 -0400

    Added csv_to_sql_hosp_drgcodes() function, included drgcodes description data in sql_hosp_admissions_to_predict_dischtime() prompt, and displayed ratio length of stay

--- llm.py
+++ llm.py
@@ -2,6 +2,7 @@
 
 import requests
 import sqlite3
+from datetime import datetime
 
 url = "http://localhost:11434/api/chat"
 
@@ -80,11 +81,50 @@ def csv_to_sql_hosp_omr():
         print(row)
     con.close()
 
+def csv_to_sql_hosp_drgcodes():
+    con = sqlite3.connect("drgcodes.db")
+    cur = con.cursor()
+    cur.execute("DROP TABLE IF EXISTS drgcodes;")
+    cur.execute("CREATE TABLE drgcodes (subject_id, hadm_id, drg_type, drg_code, description, drg_severity, drg_mortality);")
+    with open('mimic-iv-2.2/hosp/drgcodes.csv', 'r') as file:
+        total_lines = len(file.readlines())
+    with open('mimic-iv-2.2/hosp/drgcodes.csv', 'r') as file:
+        current_line_num = 1
+        while current_line_num <= total_lines:
+            current_line = file.readline().split(',')
+            if current_line_num > 1:
+                print(current_line_num)
+                print(current_line)
+                subject_id = current_line[0]
+                hadm_id = current_line[1]
+                drg_type = current_line[2]
+                drg_code = current_line[3]
+                description = ""
+                i = 4
+                while i < len(current_line) - 3:
+                    description += current_line[i]
+                    description += ","
+                    i += 1
+                description += current_line[len(current_line) - 3]
+                drg_severity = current_line[len(current_line) - 2]
+                drg_mortality = current_line[len(current_line) - 1]
+                drg_mortality = drg_mortality.split('\n')[0]
+                cur.execute("INSERT INTO drgcodes (subject_id, hadm_id, drg_type, drg_code, description, drg_severity, drg_mortality) VALUES (?, ?, ?, ?, ?, ?, ?);",
+                            (subject_id, hadm_id, drg_type, drg_code, description, drg_severity, drg_mortality))
+                con.commit()
+            current_line_num += 1
+    cur = con.cursor()
+    cur.execute("SELECT * FROM drgcodes;")
+    for row in cur.fetchall():
+        print(row)
+    con.close()
+
 def sql_hosp_admissions_to_predict_dischtime():
     con = sqlite3.connect("admissions.db")
     cur = con.cursor()
     cur.execute("SELECT * FROM admissions;")
     for row in cur.fetchall():
+        subject_id = row[0]
         admittime = row[2]
         admission_type = row[5]
         admission_location = row[7]
@@ -93,21 +133,33 @@ def sql_hosp_admissions_to_predict_dischtime():
         marital_status = row[11]
         race = row[12]
         edregtime = row[13]
+        con2 = sqlite3.connect("drgcodes.db")
+        cur2 = con2.cursor()
+        cur2.execute("SELECT description FROM drgcodes WHERE subject_id = ?;", (subject_id,))
+        description = ""
+        for row2 in cur2.fetchall():
+            description += row2[0]
         predicted_dischtime = llama3(
-            f'''Consider a patient whose admission to the hospital is at {admittime}, 
-            admission type is {admission_type}, location of admission is 
-            {admission_location}, insurance status is {insurance}, 
-            language is {language}, marital status is {marital_status}, 
-            race is {race}, and emergency room admission is at {edregtime}. 
-            What is the patient's discharge time from the hospital? The format of your answer 
-            should be in YYYY-MM-DD HH:MM:SS. Do not provide any information except for that 
-            answer.'''
+            f'''Consider a patient whose admission to the hospital is at {admittime}, admission 
+            type is {admission_type}, location of admission is {admission_location}, insurance 
+            status is {insurance}, language is {language}, marital status is {marital_status}, 
+            race is {race}, emergency room admission is at {edregtime}, and description is 
+            {description}. Predict the patient's discharge time from the hospital. The format 
+            of your answer should be in YYYY-MM-DD HH:MM:SS. Do not provide any information 
+            except for that answer.'''
         )
-        dischtime = row[3]
+        admittime = datetime.strptime(admittime, '%Y-%m-%d %H:%M:%S')
+        dischtime = datetime.strptime(row[3], '%Y-%m-%d %H:%M:%S')
+        predicted_dischtime = datetime.strptime(predicted_dischtime, '%Y-%m-%d %H:%M:%S')
+        predicted_length_of_stay = predicted_dischtime - admittime
+        length_of_stay = dischtime - admittime
+        print(f"Admittime: {admittime}")
         print(f"Predicted dischtime: {predicted_dischtime}")
         print(f"Dischtime: {dischtime}")
+        print(f"Ratio length of stay: {predicted_length_of_stay / length_of_stay}")
     con.close()
 
 # csv_to_sql_hosp_admissions()
 # csv_to_sql_hosp_omr()
+# csv_to_sql_hosp_drgcodes()
 sql_hosp_admissions_to_predict_dischtime()
